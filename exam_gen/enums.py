7b68d08da0524f094f6e66f3d974140b1fea2302
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-06-29 18:32:42 +0300

    defined Topic enum

---
+++ exam_gen/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Topic(Enum):
+    ALGORITHMS_AND_COMPLEXITY = "АЛГОРИТМИ ТА ОБЧИСЛЮВАЛЬНА СКЛАДНІСТЬ"
+    COMPUTER_SYSTEM_ARCHITECTURE = "АРХІТЕКТУРА ОБЧИСЛЮВАЛЬНИХ СИСТЕМ"
+    DATABASES_AND_DATAWAREHOUSES = "БАЗИ ТА СХОВИЩА ДАНИХ"
+    SOFTWARE_ENGINEERING = "ІНЖЕНЕРІЯ СИСТЕМ І ПРОГРАМНОГО ЗАБЕЗПЕЧЕННЯ"
+    CYBERSECURITY_AND_INFORMATION_PROTECTION = "КІБЕРБЕЗПЕКА ТА ЗАХИСТ ІНФОРМАЦІЇ"
+    MATHEMATICS_IN_IT = "МАТЕМАТИКА В ІТ"
+    NETWORKS_AND_DATA_COMMUNICATION = "МЕРЕЖІ ТА ОБМІН ДАНИМИ"
+    OPERATING_SYSTEMS = "ОПЕРАЦІЙНІ СИСТЕМИ"
+    PROGRAMMING_LANGUAGES_BASICS = "ОСНОВИ МОВ ПРОГРАМУВАННЯ"
+    ARTIFICIAL_INTELLIGENCE = "ШТУЧНИЙ ІНТЕЛЕКТ"
